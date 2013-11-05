7902079fdeaa4acf190a97be9c5849a702f5fc21
Jose Angel Concepcion <alu0100697414@ull.edu.es>
2013-11-05 17:40:01 +0000

    Implementacion del test para la suma de dos matrices

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -1,8 +1,14 @@
-require 'matriz.rb'
+require './lib/matriz.rb'
 
 describe Matriz do
 
   before :each do
     @matriz = Matriz.new([[1,2,3],[1,2,3],[1,2,3]])
   end
+
+  it "Se debe poder sumar dos matrices" do
+  @aux = Matriz.new([[1,2,3],[1,2,3],[1,2,3]])
+  (@matriz + @aux).matriz.should == [[2,4,6],[2,4,6],[2,4,6]]
+  end
+
 end
