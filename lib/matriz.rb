ff742baee56e6f052f358ff526afb7492ff2860a
Noe Campos <alu0100622492@ull.edu.es>
2013-11-05 18:05:24 +0000

    Implementado el metodo resta

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,9 +1,7 @@
 class Matriz
   
-  attr_accessor :filas, :columnas, :matriz
- 
-  include Comparable
- 
+  attr_accesor :filas, :columnas, :matriz
+    
   def initialize(m)
     @filas=m.size
     @columnas=m[1].size
@@ -26,5 +24,20 @@ class Matriz
 	Matriz.new(m_copia)
   end
   
+  #Resta de matrices
+  
+  def -(other)
+    i=0
+      m_copia = @matriz
+	while i < @filas
+	  j=0
+	    while j < @columnas
+	      m_copia[i][j] = m_copia[i][j] - other.matriz[i][j]
+	      j += 1
+	    end
+	    i += 1
+	end
+	Matriz.new(m_copia)
+  end
   
 end
