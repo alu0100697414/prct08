99aee2cf486c2c7d75d84f0379f2f1f3162a88f1
Noe Campos <alu0100622492@ull.edu.es>
2013-11-05 18:30:47 +0000

    Implementado el metodo multiplicacion

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,6 +1,6 @@
 class Matriz
   
-  attr_accesor :filas, :columnas, :matriz
+  attr_accessor :filas, :columnas, :matriz
     
   def initialize(m)
     @filas=m.size
@@ -41,3 +41,26 @@ class Matriz
   end
   
 end
+
+  #Multiplicacion de matrices
+
+  def * (other)
+    i=0
+      m_copia = Array.new(@filas) {Array.new(other.columnas,0)}
+      while i < @filas
+	    j = 0
+	      while j < other.columnas
+		  m_copia[i][j] = 0
+		  k = 0
+		    while k < @columnas
+		      m_copia[i][j] = m_copia[i][j] + (matriz[i][k]*other.matriz[k][j])
+		      k += 1
+		    end
+		    j +=1
+	      end
+	      i +=1
+      end
+      Matriz.new(m_copia)
+  end
+end
+  
