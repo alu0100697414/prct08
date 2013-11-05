1dad1715ce5fa67aa426174a0fa698753c8c7790
Noe Campos <alu0100622492@ull.edu.es>
2013-11-05 17:54:33 +0000

    Implementacion del metodo suma

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -7,4 +7,22 @@ class Matriz
     @columnas=m[1].size
     @matriz = m
   end
+  
+  #Suma de matrices
+  
+  def + (other)
+    i=0
+      m_copia = @matriz
+	while i < @filas
+	  j=0
+	  while j < @columnas
+	    m_copia[i][j] = m_copia[i][j] + other.matriz[i][j]
+	      j += 1
+	  end
+	  i += 1
+	end
+	Matriz.new(m_copia)
+  end
+  
+  
 end
